20ecdb657c55be746e32caa13b6de215ce1e01bf
Fida <iwanderuu@gmail.com>
2022-03-26 17:16:42 +0800

    remove chrome-flags ulvim; change env

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -13,7 +13,7 @@ export GOPATH="${XDG_DATA_HOME:-$HOME/.local/share}/go"
 export PATH=$PATH:$HOME/.cargo/bin
 export HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/history"
 
-# export http_proxy=http://192.168.10.1:7890
-# export https_proxy=http://192.168.10.1:7890
-export http_proxy=http://127.0.0.1:7890
-export https_proxy=http://127.0.0.1:7890
+export http_proxy=http://192.168.10.1:7890
+export https_proxy=http://192.168.10.1:7890
+# export http_proxy=http://127.0.0.1:7890
+# export https_proxy=http://127.0.0.1:7890
