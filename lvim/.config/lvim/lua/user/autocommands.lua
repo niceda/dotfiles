24cc5d69ad4165418a4c2992d66b45e5baeb94c7
Fida <fida@ximix.localdomain>
2022-06-07 16:37:02 +0800

    remove autocommands

--- lvim/.config/lvim/lua/user/autocommands.lua
+++
@@ -1,8 +0,0 @@
-lvim.autocommands.custom_groups = {
-	{ "BufReadPost", [[* if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif"]] },
-	{
-		"Filetype",
-		"rust",
-		"nnoremap <leader>m <cmd>lua require('lvim.core.terminal')._exec_toggle({cmd='cargo build;read',count=2,direction='float'})<CR>",
-	},
-}
